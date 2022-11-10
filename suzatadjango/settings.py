6ccb4f52307d74401a40df55f6184522a108df93
parasraipeacenepal <parasrai@gmail.com>
2022-11-10 19:13:26 +0545

    Added requirement

--- suzatadjango/settings.py
+++ suzatadjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,8 +101,14 @@ WSGI_APPLICATION = 'suzatadjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':  os.environ.get('DB_ENGINE'),
+        'NAME':  os.environ.get('DB_NAME'),
+
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+
     }
 }
 
